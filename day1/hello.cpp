0021bff4441f81b7cdf7c78be75dcf0ad13dd4fb
shoebsift360 <shoebsifat360@gmail.com>
2022-06-09 11:49:47 +0600

    add cpp code

--- day1/hello.cpp
+++ day1/hello.cpp
@@ -1,8 +1,27 @@
 #include<iostream>
 using namespace std;
 
+
+int fibo(int n){
+
+    if(n==0){
+        return n;
+    }
+    else if (n==1)
+    {
+        return n;
+    }
+    else{
+        return (fibo(n-1)+fibo(n-2));
+    }
+}
+
 int main()
 {
-    cout<<"helo c++";
+    for (int i = 0; i <= 10; i++)
+    {
+        cout<<fibo(i)<<",";
+    }
+    
     return 0;
 }
